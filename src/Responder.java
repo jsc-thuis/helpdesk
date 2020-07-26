65ffc096849301adfa7cdde02eee03e9c5a57ac8
marcdc-tm <marc.decaluwe@thomasmore.be>
2020-07-26 22:29:19 +0200

    2de versie

--- src/Responder.java
+++ src/Responder.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,11 +10,32 @@
  */
 public class Responder
 {
+    private Random randomGenerator;
+    private ArrayList<String> responses;
+
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        randomGenerator = new Random();
+        responses = new ArrayList<String>();
+        fillResponses();
+    }
+
+    private void fillResponses() {
+        responses.add("That sounds interesting. Tell me more...");
+        responses.add("That sounds odd. Could you describe that problem in more detail?");
+        responses.add("No other customer has ever complained about this before. \n" +
+                      "What is your system configuration?");
+        responses.add("That's a known problem with Vista. Windows 7 is much better.");
+        responses.add("I need a bit more information on that.");
+        responses.add("Have you checked that you do not have a dll conflict?");
+        responses.add("That is explained in the manual. Have you read the manual?");
+        responses.add("Your description is a bit wishy-washy. Have you got an expert\n" +
+                      "there with you who could describe this more precisely?");
+        responses.add("That's not a bug, it's a feature!");
+        responses.add("Could you elaborate on that?");
     }
 
     /**
@@ -20,6 +44,6 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
-    }
+        int index = randomGenerator.nextInt(responses.size());
+        return responses.get(index);    }
 }
