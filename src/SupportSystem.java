c0e40b06a10661174cfc8981d21b2443df889efc
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-02-10 16:49:33 +0100

    antwoorden map toegevoegd

--- src/SupportSystem.java
+++ src/SupportSystem.java
@@ -17,22 +17,22 @@ public class SupportSystem
 {
     private InputReader reader;
     private Responder responder;
-    private final String NAME_COMPANY = "DodgySoft";
-    private final String NAME_SYSTEM = NAME_COMPANY + " Technical Support System";
-    private final String QUIT_COMMAND = "bye";
-    private final String WELCOME = "Welcome to the " + NAME_SYSTEM + ".\n\n" +
-                                    "Please tell us about your problem.\n" +
-                                    "We will assist you with any problem you might have.\n" +
-                                    "Please type '" + QUIT_COMMAND + "' to exit our system.";
-    private final String GOODBYE = "Nice talking to you. Bye...";
+    private final String QUIT_COMMAND;
+    private final String WELCOME;
+    private final String GOODBYE;
 
     /**
      * Creates a technical support system.
      */
-    public SupportSystem()
+    public SupportSystem(String companyName)
     {
         reader = new InputReader();
         responder = new Responder();
+        ServiceReader serviceReader = new ServiceReader();
+        Service service = serviceReader.getService(companyName);
+        QUIT_COMMAND = service.getValue(Service.COMMAND);
+        WELCOME = service.getValue(Service.WELCOME);
+        GOODBYE = service.getValue(Service.GOODBYE);
     }
 
     /**
--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        SupportSystem s = new SupportSystem();
+        SupportSystem s = new SupportSystem("Test");
         s.start();
     }
 }
